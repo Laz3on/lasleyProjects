fec920d244c2d5aa904daff7aeb6a0d1d06cc1ec
Chris <chris.lasley@gmail.com>
2020-01-13 05:56:59 -0500

    Changes to DB, Projects.HTML
    Updated DB with a table for a projects list, updated projects.html for
    displaying the projects, and allow the user to add, update and delete projects from the list.

--- app.py
+++ app.py
@@ -22,6 +22,19 @@ class ActionsList(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
+class ProjectsList(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    projectName = db.Column(db.String(200), nullable=False)
+    projectDetails = db.Column(db.String(200), nullable=False)
+    owner = db.Column(db.String(200), nullable=False)
+    priority = db.Column(db.String(200), nullable=False)
+    startDate = db.Column(db.String(200), nullable=False)
+    estimatedCompletionDate = db.Column(db.String(200), nullable=False)
+    dateCreated = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Project %r>' % self.id
+
 
 @app.route('/actions', methods=['POST', 'GET'])
 def actions():
@@ -89,10 +102,64 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/projects')
+@app.route('/projects', methods=['POST', 'GET'])
 def projects():
-    return render_template('projects.html')
+    if request.method == 'POST':
+        projectName = request.form['projectName']
+        projectDetails = request.form['projectDetails']
+        owner = request.form['owner']
+        priority = request.form['priority']
+        startDate = request.form['startDate']
+        estimatedCompletionDate = request.form['estimatedCompletionDate']
+        # dateCreated = request.form['dateCreated']
+        new_project = ProjectsList(projectName=projectName, projectDetails=projectDetails, owner=owner, priority=priority,
+                               startDate=startDate, estimatedCompletionDate=estimatedCompletionDate)
 
+        try:
+            db.session.add(new_project)
+            db.session.commit()
+            return redirect('/projects')
+
+        except:
+
+            return 'There was an issue adding your project'
+
+    else:
+        projects = ProjectsList.query.order_by(ProjectsList.dateCreated).all()
+        return render_template("/projects.html", projects=projects)
+
+
+@app.route('/projects/delete/<int:id>')
+def project_delete(id):
+    project_to_delete = ProjectsList.query.get_or_404(id)
+
+    try:
+        db.session.delete(project_to_delete)
+        db.session.commit()
+        return redirect('/projects')
+    except:
+        return 'There was a problem deleting that project'
+
+
+@app.route('/projects/projectUpdate/<int:id>', methods=['GET', 'POST'])
+def project_update(id):
+    project_to_update = ProjectsList.query.get_or_404(id)
+    if request.method == 'POST':
+        project_to_update.projectName = request.form['projectName']
+        project_to_update.projectDetails = request.form['projectDetails']
+        project_to_update.owner = request.form['owner']
+        project_to_update.priority = request.form['priority']
+        project_to_update.startDate = request.form['startDate']
+        project_to_update.estimatedCompletionDate = request.form['estimatedCompletionDate']
+
+        try:
+            db.session.commit()
+            return redirect('/projects')
+        except:
+            return 'There was a problem updating your project'
+
+    else:
+        return render_template('projectUpdate.html', project_to_update=project_to_update)
 
 @app.route('/about')
 def about():
